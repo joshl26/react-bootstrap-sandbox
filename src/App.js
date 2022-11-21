2bb3f3564193986b7cc636ea653b2369a6a6e0e9
joshl26 <joshl26@hotmail.com>
2022-11-21 12:56:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import ToastNotification from "./components/ToastNotification";
+import NavBarBasic from "./components/NavBarBasic";
+import ResponsiveNavBar from "./components/ResponsiveNavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBarBasic />
+      <ResponsiveNavBar />
+
       <ToastNotification />
+      <div className="spacer" />
     </div>
   );
 }
