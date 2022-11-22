4d5fcc64780109fc2c76fa8280bb4b96092ffef6
joshl26 <joshl26@hotmail.com>
2022-11-21 19:11:57 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import "./App.css";
 import ToastNotification from "./components/ToastNotification";
 import NavBarBasic from "./components/NavBarBasic";
 import ResponsiveNavBar from "./components/ResponsiveNavBar";
+import ResponsiveGrid from "./components/grid/ResponsiveGrids";
 
 function App() {
   return (
     <div className="App">
       <NavBarBasic />
       <ResponsiveNavBar />
-
+      <ResponsiveGrid />
       <ToastNotification />
       <div className="spacer" />
     </div>
